1f2e21cdb3007811e7498f7bcb3ef58a108f71c5
abhi <assomvanshi7@gmail.com>
2021-11-07 17:23:24 +0530

    angular blog project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BlogsComponent } from './blogs/blogs.component';
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'blog', component: BlogsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
